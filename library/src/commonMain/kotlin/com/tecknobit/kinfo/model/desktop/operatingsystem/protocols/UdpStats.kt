83a71f93d80b53974b08da4fdc7d790567860a80
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2025-10-16 10:23:41 +0200

    Working on the operating system part, writing the internet_protocol_stats.md page, completed the tcp_stats.md and udp_stats.md pages

--- library/src/commonMain/kotlin/com/tecknobit/kinfo/model/desktop/operatingsystem/protocols/UdpStats.kt
+++ library/src/commonMain/kotlin/com/tecknobit/kinfo/model/desktop/operatingsystem/protocols/UdpStats.kt
@@ -2,7 +2,7 @@ package com.tecknobit.kinfo.model.desktop.operatingsystem.protocols
 
 /**
  * `UdpStats` Represents the statistics for the UDP protocol, including the number of datagrams sent and received,
- * as well as any errors or datagrams with no port.
+ * as well as any errors or datagrams with no port
  *
  * @author N7ghtm4r3
  *
@@ -11,22 +11,22 @@ package com.tecknobit.kinfo.model.desktop.operatingsystem.protocols
 interface UdpStats {
 
     /**
-     * `datagramsSent` The total number of UDP datagrams sent.
+     * `datagramsSent` The total number of UDP datagrams sent
      */
     val datagramsSent: Long
 
     /**
-     * `datagramsReceived` The total number of UDP datagrams received.
+     * `datagramsReceived` The total number of UDP datagrams received
      */
     val datagramsReceived: Long
 
     /**
-     * `datagramsNoPort` The total number of UDP datagrams that did not have a corresponding port to route to.
+     * `datagramsNoPort` The total number of UDP datagrams that did not have a corresponding port to route to
      */
     val datagramsNoPort: Long
 
     /**
-     * `datagramsReceivedErrors` The total number of UDP datagrams that were received with errors (e.g., checksum errors).
+     * `datagramsReceivedErrors` The total number of UDP datagrams that were received with errors (e.g., checksum errors)
      */
     val datagramsReceivedErrors: Long
 
