addc43560318f18116e18c33bbdb786eebf2794d
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2026-04-15 18:03:30 +0200

    feat(Printer) integrated the new Printer API for desktop target

---
+++ library/src/commonMain/kotlin/com/tecknobit/kinfo/model/desktop/hardware/Printer.kt
@@ -0,0 +1,94 @@
+package com.tecknobit.kinfo.model.desktop.hardware
+
+/**
+ * This interface represents a printer and provides details about name, driver name,
+ * description, current status, and connection information
+ *
+ * It also offers information about whether the printer is the default printer,
+ * whether it is local or network-based, and the reason for its current status
+ *
+ * Integration in [oshi - #3078](https://github.com/oshi/oshi/pull/3078)
+ *
+ * @author N7ghtm4r3
+ *
+ * @since 1.0.6
+ */
+interface Printer {
+
+    /**
+     * `name` The name of the printer
+     */
+    val name: String
+
+    /**
+     * `driverName` The driver name or make/model of the printer
+     */
+    val driverName: String
+
+    /**
+     * `description` The user-friendly description of the printer
+     */
+    val description: String
+
+    /**
+     * `status` The current status of the printer
+     */
+    val status: PrinterStatus
+
+    /**
+     * `statusReason` The reason for the current printer status, if available
+     * (e.g., "Paper Jam", "media-empty")
+     */
+    val statusReason: String
+
+    /**
+     * `isDefault` Indicates whether this is the default printer
+     */
+    val isDefault: Boolean
+
+    /**
+     * `isLocal` Indicates whether this is a local printer
+     * (as opposed to a network printer)
+     */
+    val isLocal: Boolean
+
+    /**
+     * `portName` The port name or device URI of the printer
+     */
+    val portName: String
+}
+
+/**
+ * `PrinterStatus` Represents the different states a printer can be
+ *
+ * @author N7ghtm4r3
+ *
+ * @since 1.0.6
+ */
+enum class PrinterStatus {
+
+    /**
+     * `IDLE` The printer is currently idle
+     */
+    IDLE,
+
+    /**
+     * `PRINTING` The printer is currently printing
+     */
+    PRINTING,
+
+    /**
+     * `ERROR` The printer is currently facing an error
+     */
+    ERROR,
+
+    /**
+     * `OFFLINE` The printer is currently offline
+     */
+    OFFLINE,
+
+    /**
+     * `UNKNOWN` The printer is currently in an unknown status
+     */
+    UNKNOWN
+}
---
+++ library/src/jvmMain/kotlin/com/tecknobit/kinfo/model/hardware/PrinterImpl.kt
@@ -0,0 +1,30 @@
+package com.tecknobit.kinfo.model.hardware
+
+import com.tecknobit.kinfo.model.desktop.hardware.Printer
+import com.tecknobit.kinfo.model.desktop.hardware.PrinterStatus
+
+/**
+ * Implementation of the `Printer` interface.
+ *
+ * This class provides details about a printer, including its name, driver name,
+ * description, current status, and connection information
+ *
+ * It also offers information about whether the printer is the default printer,
+ * whether it is local or network-based, and the reason for its current status
+ *
+ * Integration in [oshi - #3078](https://github.com/oshi/oshi/pull/3078)
+ *
+ * @author N7ghtm4r3
+ *
+ * @since 1.0.6
+ */
+class PrinterImpl(
+    override val name: String,
+    override val driverName: String,
+    override val description: String,
+    override val status: PrinterStatus,
+    override val statusReason: String,
+    override val isDefault: Boolean,
+    override val isLocal: Boolean,
+    override val portName: String,
+) : Printer
