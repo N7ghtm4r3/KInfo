02e305e6a995501f700d3b8857c87cc301d6c5a3
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2026-08-25 08:43:28 +0200

    feat(NetworkParams): working on integrating oshi features, integrated the routes information retrieval

--- library/src/commonMain/kotlin/com/tecknobit/kinfo/model/desktop/operatingsystem/protocols/NetworkParams.kt
+++ library/src/commonMain/kotlin/com/tecknobit/kinfo/model/desktop/operatingsystem/protocols/NetworkParams.kt
@@ -1,37 +1,44 @@
 package com.tecknobit.kinfo.model.desktop.operatingsystem.protocols
 
 /**
- * `NetworkParams` Represents the network configuration parameters of a device,
- * providing details about the host name, domain name, DNS servers, and default gateways
- * for both IPv4 and IPv6
+ * The `NetworkParams` interface defines the contract to represent the network configuration of a device
+ *
+ * It provides the host name, domain name, DNS servers, default gateways, and routing table entries for `IPv4` and
+ * `IPv6`
  *
  * @author N7ghtm4r3 - Tecknobit
  */
 interface NetworkParams {
 
     /**
-     * `hostName` The name of the device on the network (e.g., "MyComputer")
+     * `hostName` the name of the device on the network, such as `MyComputer`
      */
     val hostName: String
 
     /**
-     * `domainName` The domain name of the network or the device (e.g., "example.com")
+     * `domainName` the domain name of the network or device, such as `example.com`
      */
     val domainName: String
 
     /**
-     * `dnsServers` A list of DNS server addresses used by the device for domain name resolution
+     * `dnsServers` the DNS server addresses used by the device for domain name resolution
      */
     val dnsServers: Array<String>
 
     /**
-     * `ipv4DefaultGateway` The default gateway for IPv4 traffic, typically the router's IP address
+     * `ipv4DefaultGateway` the default gateway for `IPv4` traffic, or an empty string when not defined
      */
     val ipv4DefaultGateway: String
 
     /**
-     * `ipv6DefaultGateway` The default gateway for IPv6 traffic, typically the router's IPv6 address
+     * `ipv6DefaultGateway` the default gateway for `IPv6` traffic, or an empty string when not defined
      */
     val ipv6DefaultGateway: String
 
+    /**
+     * `routes` the `IPv4` and `IPv6` entries available in the operating system routing table
+     *
+     * @since 1.1.0
+     */
+    val routes: List<IPRoute>
 }
