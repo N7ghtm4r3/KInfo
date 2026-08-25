9cd3909f441fdd6c89db794ca59768443754da40
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2026-08-26 00:42:38 +0200

    feat(macos): mapped main interfaces

--- library/src/macosMain/kotlin/com/tecknobit/kinfo/operatingsystem/MacOsOperatingSystemImpl.kt
+++ library/src/macosMain/kotlin/com/tecknobit/kinfo/operatingsystem/MacOsOperatingSystemImpl.kt
@@ -1,5 +1,6 @@
-package com.tecknobit.kinfo.model.desktop.macos.operatingsystem
+package com.tecknobit.kinfo.operatingsystem
 
+import com.tecknobit.kinfo.model.desktop.macos.operatingsystem.MacOsOperatingSystem
 import com.tecknobit.kinfo.model.desktop.operatingsystem.OSDesktopWindow
 import com.tecknobit.kinfo.model.desktop.operatingsystem.OSFileStore
 import com.tecknobit.kinfo.model.desktop.operatingsystem.OSSession
@@ -11,25 +12,15 @@ import com.tecknobit.kinfo.model.desktop.operatingsystem.protocols.IPRoute
 import com.tecknobit.kinfo.model.desktop.operatingsystem.protocols.TcpStats
 import com.tecknobit.kinfo.model.desktop.operatingsystem.protocols.UdpStats
 
-interface MacOsOperatingSystem {
-
-    val operatingSystemVersion: OSVersionInfo
-
-    val statfs: OSFileStore
-
-    val cgWindowId: OSDesktopWindow
-
-    val utmpx: OSSession
-
-    val procTaskAllInfo: OSProcess
-
-    val procThreadInfo: OSThread
-
-    val socketFdInfo: IPConnection
-
-    val rtMsgHdr2: IPRoute
-
-    val tcpStat: TcpStats
-
-    val udpStat: UdpStats
-}
+data class MacOsOperatingSystemImpl(
+    override val operatingSystemVersion: OSVersionInfo,
+    override val statfs: OSFileStore,
+    override val cgWindowId: OSDesktopWindow,
+    override val utmpx: OSSession,
+    override val procTaskAllInfo: OSProcess,
+    override val procThreadInfo: OSThread,
+    override val socketFdInfo: IPConnection,
+    override val rtMsgHdr2: IPRoute,
+    override val tcpStat: TcpStats,
+    override val udpStat: UdpStats
+) : MacOsOperatingSystem
