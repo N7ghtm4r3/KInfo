0ad8077a5f54d194b181e1e0bda1d7444653fefb
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2026-08-26 21:10:53 +0200

    Add macOS platform support and operating system version details

--- library/src/macosMain/kotlin/com/tecknobit/kinfo/MacOsInfoImpl.kt
+++ library/src/macosMain/kotlin/com/tecknobit/kinfo/MacOsInfoImpl.kt
@@ -3,12 +3,31 @@ package com.tecknobit.kinfo
 import com.tecknobit.kinfo.model.desktop.macos.MacOsInfo
 import com.tecknobit.kinfo.model.desktop.macos.hardware.MacOsHardware
 import com.tecknobit.kinfo.model.desktop.macos.operatingsystem.MacOsOperatingSystem
+import com.tecknobit.kinfo.operatingsystem.MacOsOperatingSystemImpl
+import platform.Foundation.NSProcessInfo
 
+/**
+ * The `MacOsInfoImpl` class is useful to provide the information about the current macOS device
+ *
+ * @author N7ghtm4r3 - Tecknobit
+ *
+ * @since 1.1.0
+ */
 class MacOsInfoImpl : MacOsInfo {
 
+    /**
+     * `operatingSystem` the information about the current macOS operating system
+     *
+     * @since 1.1.0
+     */
     override val operatingSystem: MacOsOperatingSystem
-        get() = TODO("Not yet implemented")
+        get() = MacOsOperatingSystemImpl(
+            processInfo = NSProcessInfo.processInfo
+        )
 
+    /**
+     * `hardware` the information about the current macOS hardware
+     */
     override val hardware: MacOsHardware
         get() = TODO("Not yet implemented")
 }
