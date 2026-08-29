4d0fc10f90d965311892ff853ba8c976799bdb44
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2026-08-29 16:27:01 +0200

    feat(MacOsOsProcessImpl): working on implementation

---
+++ library/src/commonMain/kotlin/com/tecknobit/kinfo/model/desktop/macos/operatingsystem/MacOsOSProcess.kt
@@ -0,0 +1,5 @@
+package com.tecknobit.kinfo.model.desktop.macos.operatingsystem
+
+import com.tecknobit.kinfo.model.desktop.common.operatingsystem.processes.OSProcess
+
+interface MacOsOSProcess : OSProcess
