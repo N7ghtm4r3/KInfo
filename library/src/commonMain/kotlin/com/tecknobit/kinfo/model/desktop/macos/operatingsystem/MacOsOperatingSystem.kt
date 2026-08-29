267a075b25eefe85207128a8b48828b2d9cba9d3
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2026-08-29 14:07:14 +0200

    feat(MacOsOperatingSystemImpl): implemented the visibleWindows and allWindows properties

--- library/src/commonMain/kotlin/com/tecknobit/kinfo/model/desktop/macos/operatingsystem/MacOsOperatingSystem.kt
+++ library/src/commonMain/kotlin/com/tecknobit/kinfo/model/desktop/macos/operatingsystem/MacOsOperatingSystem.kt
@@ -1,9 +1,6 @@
 package com.tecknobit.kinfo.model.desktop.macos.operatingsystem
 
-import com.tecknobit.kinfo.model.desktop.operatingsystem.OSDesktopWindow
-import com.tecknobit.kinfo.model.desktop.operatingsystem.OSFileStore
 import com.tecknobit.kinfo.model.desktop.operatingsystem.OSSession
-import com.tecknobit.kinfo.model.desktop.operatingsystem.OSVersionInfo
 import com.tecknobit.kinfo.model.desktop.operatingsystem.processes.OSProcess
 import com.tecknobit.kinfo.model.desktop.operatingsystem.processes.OSThread
 import com.tecknobit.kinfo.model.desktop.operatingsystem.protocols.IPConnection
@@ -11,25 +8,67 @@ import com.tecknobit.kinfo.model.desktop.operatingsystem.protocols.IPRoute
 import com.tecknobit.kinfo.model.desktop.operatingsystem.protocols.TcpStats
 import com.tecknobit.kinfo.model.desktop.operatingsystem.protocols.UdpStats
 
+/**
+ * The `MacOsOperatingSystem` interface defines the contract to provide macOS operating system details
+ *
+ * @author N7ghtm4r3 - Tecknobit
+ *
+ * @since 1.1.0
+ */
 interface MacOsOperatingSystem {
 
-    val operatingSystemVersion: OSVersionInfo
+    /**
+     * `operatingSystemVersion` the version information of the current macOS operating system
+     */
+    val operatingSystemVersion: MacOsVersionInfo
 
-    val statfs: OSFileStore
+    /**
+     * `statfs` the file store information of the current macOS operating system
+     */
+    val statfs: MacOsFileStore
 
-    val cgWindowId: OSDesktopWindow
+    /**
+     * `visibleWindows` the desktop windows currently visible on macOS
+     */
+    val visibleWindows: List<MacOsDesktopWindow>
 
+    /**
+     * `allWindows` the desktop windows available on macOS
+     */
+    val allWindows: List<MacOsDesktopWindow>
+
+    /**
+     * `utmpx` the current macOS session information
+     */
     val utmpx: OSSession
 
+    /**
+     * `procTaskAllInfo` the current macOS process information
+     */
     val procTaskAllInfo: OSProcess
 
+    /**
+     * `procThreadInfo` the current macOS thread information
+     */
     val procThreadInfo: OSThread
 
+    /**
+     * `socketFdInfo` the current macOS `IP` connection information
+     */
     val socketFdInfo: IPConnection
 
+    /**
+     * `rtMsgHdr2` the current macOS `IP` route information
+     */
     val rtMsgHdr2: IPRoute
 
+    /**
+     * `tcpStat` the current macOS `TCP` statistics
+     */
     val tcpStat: TcpStats
 
+    /**
+     * `udpStat` the current macOS `UDP` statistics
+     */
     val udpStat: UdpStats
 }
