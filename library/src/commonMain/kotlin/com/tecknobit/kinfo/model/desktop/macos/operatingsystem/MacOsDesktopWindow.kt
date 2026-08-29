8adb2c1adc3dbbc1c6645a17a467aa502246e04d
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2026-08-29 14:07:14 +0200

    feat(MacOsOperatingSystemImpl): implemented the visibleWindows and allWindows properties

---
+++ library/src/commonMain/kotlin/com/tecknobit/kinfo/model/desktop/macos/operatingsystem/MacOsDesktopWindow.kt
@@ -0,0 +1,14 @@
+package com.tecknobit.kinfo.model.desktop.macos.operatingsystem
+
+import com.tecknobit.kinfo.model.desktop.operatingsystem.OSDesktopWindow
+
+/**
+ * The `MacOsDesktopWindow` interface defines the contract to provide the details of a desktop window on macOS
+ *
+ * @author N7ghtm4r3 - Tecknobit
+ *
+ * @see OSDesktopWindow
+ *
+ * @since 1.1.0
+ */
+interface MacOsDesktopWindow : OSDesktopWindow
