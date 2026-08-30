08bd3aabd14ed07e0f01f8f5b706e1127059b544
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2026-08-30 12:27:56 +0200

    Document macOS native mappers and process models

--- library/src/commonMain/kotlin/com/tecknobit/kinfo/annotations/Resolver.kt
+++ library/src/commonMain/kotlin/com/tecknobit/kinfo/annotations/Resolver.kt
@@ -2,6 +2,13 @@ package com.tecknobit.kinfo.annotations
 
 import kotlin.annotation.AnnotationTarget.FUNCTION
 
+/**
+ * The `Resolver` annotation is useful to mark methods responsible for resolving platform-native values
+ *
+ * @author N7ghtm4r3 - Tecknobit
+ *
+ * @since 1.1.0
+ */
 @Retention(value = AnnotationRetention.SOURCE)
 @Target(allowedTargets = [FUNCTION])
 annotation class Resolver
