fd535cf5b3dd8591d8ba17402667b55f1ce66394
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2026-09-05 14:32:42 +0200

    Refactor project structure and remove obsolete code

---
+++ library/src/commonMain/kotlin/com/tecknobit/kinfo/model/desktop/macos/operatingsystem/MacOsIpRoute.kt
@@ -0,0 +1,15 @@
+package com.tecknobit.kinfo.model.desktop.macos.operatingsystem
+
+import com.tecknobit.kinfo.model.desktop.common.operatingsystem.protocols.IPRoute
+
+/**
+ * The `MacOsIpRoute` interface defines the contract to represent an entry in the macOS routing
+ * table
+ *
+ * @author N7ghtm4r3 - Tecknobit
+ *
+ * @see IPRoute
+ *
+ * @since 1.1.0
+ */
+interface MacOsIpRoute : IPRoute
