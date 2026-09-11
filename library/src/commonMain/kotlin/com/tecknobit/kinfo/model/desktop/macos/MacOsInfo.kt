24b5406b200e99240dd66f8e7332892b03df3900
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2026-09-11 23:53:48 +0200

    feat(MacOsBaseboardMapper): implemented the mapper for MacOsBaseboard

--- library/src/commonMain/kotlin/com/tecknobit/kinfo/model/desktop/macos/MacOsInfo.kt
+++ library/src/commonMain/kotlin/com/tecknobit/kinfo/model/desktop/macos/MacOsInfo.kt
@@ -3,10 +3,23 @@ package com.tecknobit.kinfo.model.desktop.macos
 import com.tecknobit.kinfo.model.desktop.macos.hardware.MacOsHardware
 import com.tecknobit.kinfo.model.desktop.macos.operatingsystem.MacOsOperatingSystem
 
+/**
+ * The `MacOsInfo` interface defines the contract to expose macOS operating system and hardware information
+ *
+ * @author N7ghtm4r3 - Tecknobit
+ *
+ * @since 1.1.0
+ */
 interface MacOsInfo {
 
+    /**
+     * `operatingSystem` the operating system information
+     */
     val operatingSystem: MacOsOperatingSystem
 
+    /**
+     * `hardware` the hardware information
+     */
     val hardware: MacOsHardware
 
 }
--- library/src/commonMain/kotlin/com/tecknobit/kinfo/model/desktop/macos/hardware/MacOsHardware.kt
+++ library/src/commonMain/kotlin/com/tecknobit/kinfo/model/desktop/macos/hardware/MacOsHardware.kt
@@ -1,47 +1,117 @@
 package com.tecknobit.kinfo.model.desktop.macos.hardware
 
+/**
+ * The `MacOsHardware` interface defines the contract to expose macOS hardware information
+ *
+ * @author N7ghtm4r3 - Tecknobit
+ *
+ * @since 1.1.0
+ */
 interface MacOsHardware {
 
+    /**
+     * `platformExpertDevice` the computer system information
+     */
     val platformExpertDevice: MacOsComputerSystem
 
+    /**
+     * `baseboardRegistryEntry` the baseboard information
+     */
     val baseboardRegistryEntry: MacOsBaseboard
 
+    /**
+     * `romRegistryEntry` the firmware information
+     */
     val romRegistryEntry: MacOsFirmware
 
+    /**
+     * `processorInfo` the central processor information
+     */
     val processorInfo: MacOsCentralProcessor
 
+    /**
+     * `processorIdentifierInfo` the processor identification information
+     */
     val processorIdentifierInfo: MacOsProcessorIdentifier
 
+    /**
+     * `processorCaches` the processor cache information
+     */
     val processorCaches: MacOsProcessorCache
 
+    /**
+     * `logicalProcessorInfo` the logical processor information
+     */
     val logicalProcessorInfo: MacOsLogicalProcessor
 
+    /**
+     * `physicalProcessorInfo` the physical processor information
+     */
     val physicalProcessorInfo: MacOsPhysicalProcessor
 
+    /**
+     * `vmStatistics` the physical memory information
+     */
     val vmStatistics: MacOsGlobalMemory
 
+    /**
+     * `swapUsage` the virtual memory and swap information
+     */
     val swapUsage: MacOsVirtualMemory
 
+    /**
+     * `powerSourceDescription` the power source information
+     */
     val powerSourceDescription: MacOsPowerSource
 
+    /**
+     * `disk` the disk information
+     */
     val disk: MacOsHWDiskStore
 
+    /**
+     * `media` the partition information
+     */
     val media: MacOsHWPartition
 
+    /**
+     * `networkInterface` the network interface information
+     */
     val networkInterface: MacOsNetworkIF
 
+    /**
+     * `displayId` the display information
+     */
     val displayId: MacOsDisplay
 
+    /**
+     * `displayService` the display information
+     */
     val displayService: MacOsDisplayInfo
 
+    /**
+     * `usbDevice` the USB device information
+     */
     val usbDevice: MacOsUsbDevice
 
+    /**
+     * `bluetoothDevice` the Bluetooth device information
+     */
     val bluetoothDevice: MacOsBluetoothDevice
 
+    /**
+     * `destination` the printer information
+     */
     val destination: MacOsPrinter
 
+    /**
+     * `audioDeviceId` the audio device information
+     */
     val audioDeviceId: MacOsSoundCard
 
+    /**
+     * `metalDevice` the graphics card information
+     */
     val metalDevice: MacOsGraphicsCard
 
 }
