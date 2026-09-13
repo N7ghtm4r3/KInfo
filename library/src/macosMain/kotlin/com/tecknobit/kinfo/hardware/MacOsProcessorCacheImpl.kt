deae7021232c769877fb13487c4385126163e09c
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2026-09-13 11:03:45 +0200

    feat(MacOsProcessorCache): implemented the native mapping

--- library/src/macosMain/kotlin/com/tecknobit/kinfo/hardware/MacOsProcessorCacheImpl.kt
+++ library/src/macosMain/kotlin/com/tecknobit/kinfo/hardware/MacOsProcessorCacheImpl.kt
@@ -3,6 +3,21 @@ package com.tecknobit.kinfo.hardware
 import com.tecknobit.kinfo.model.desktop.common.hardware.centralprocessor.CacheType
 import com.tecknobit.kinfo.model.desktop.macos.hardware.MacOsProcessorCache
 
+/**
+ * The `MacOsProcessorCacheImpl` class is useful to represent a macOS processor cache
+ *
+ * @property level The cache hierarchy level
+ * @property associativity The number of cache ways, or zero when unknown
+ * @property lineSize The cache line size in bytes, or zero when unknown
+ * @property cacheSize The cache capacity in bytes
+ * @property type The kind of content stored in the cache
+ *
+ * @author N7ghtm4r3 - Tecknobit
+ *
+ * @see MacOsProcessorCache
+ *
+ * @since 1.1.0
+ */
 data class MacOsProcessorCacheImpl(
     override val level: Byte,
     override val associativity: Byte,
