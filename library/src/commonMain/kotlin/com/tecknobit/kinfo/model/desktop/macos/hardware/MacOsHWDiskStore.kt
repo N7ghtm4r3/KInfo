9ce9477a56a96ffe357c957d676165b8d990a2fe
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2026-09-19 10:57:39 +0200

    feat(MacOsHWDiskStore): completed the native mapping

--- library/src/commonMain/kotlin/com/tecknobit/kinfo/model/desktop/macos/hardware/MacOsHWDiskStore.kt
+++ library/src/commonMain/kotlin/com/tecknobit/kinfo/model/desktop/macos/hardware/MacOsHWDiskStore.kt
@@ -2,4 +2,13 @@ package com.tecknobit.kinfo.model.desktop.macos.hardware
 
 import com.tecknobit.kinfo.model.desktop.common.hardware.storage.HWDiskStore
 
+/**
+ * The `MacOsHWDiskStore` interface defines the contract to expose macOS disk information
+ *
+ * The inherited [HWDiskStore] contract provides device identity, capacity, I/O statistics, and associated partitions
+ *
+ * @author N7ghtm4r3 - Tecknobit
+ *
+ * @since 1.1.0
+ */
 interface MacOsHWDiskStore : HWDiskStore
