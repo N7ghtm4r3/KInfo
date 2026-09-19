a40e40a4faa43b9882c50e0e467c32c15d21fcf7
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2026-09-19 10:57:39 +0200

    feat(MacOsHWDiskStore): completed the native mapping

--- library/src/commonMain/kotlin/com/tecknobit/kinfo/model/desktop/macos/hardware/MacOsHWPartition.kt
+++ library/src/commonMain/kotlin/com/tecknobit/kinfo/model/desktop/macos/hardware/MacOsHWPartition.kt
@@ -2,4 +2,13 @@ package com.tecknobit.kinfo.model.desktop.macos.hardware
 
 import com.tecknobit.kinfo.model.desktop.common.hardware.storage.HWPartition
 
+/**
+ * The `MacOsHWPartition` interface defines the contract to expose macOS partition and logical media information
+ *
+ * The inherited [HWPartition] contract provides media identity, filesystem details, capacity, and mount information
+ *
+ * @author N7ghtm4r3 - Tecknobit
+ *
+ * @since 1.1.0
+ */
 interface MacOsHWPartition : HWPartition
