9e51d24b60f5226711730d89cbb2d8a568080d39
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2026-09-20 12:22:26 +0200

    feat(MacOsNetworkIF): completed the native mapping

--- library/src/commonMain/kotlin/com/tecknobit/kinfo/model/desktop/macos/hardware/MacOsNetworkIF.kt
+++ library/src/commonMain/kotlin/com/tecknobit/kinfo/model/desktop/macos/hardware/MacOsNetworkIF.kt
@@ -2,4 +2,11 @@ package com.tecknobit.kinfo.model.desktop.macos.hardware
 
 import com.tecknobit.kinfo.model.desktop.common.hardware.NetworkIF
 
+/**
+ * The `MacOsNetworkIF` interface defines the contract to expose macOS network interface information through [NetworkIF]
+ *
+ * @author N7ghtm4r3 - Tecknobit
+ *
+ * @since 1.1.0
+ */
 interface MacOsNetworkIF : NetworkIF
