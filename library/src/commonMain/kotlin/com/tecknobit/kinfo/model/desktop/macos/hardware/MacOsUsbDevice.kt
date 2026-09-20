c3048ef084d1bf73744e03325dd508a85b0071b8
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2026-09-20 09:38:04 +0200

    feat(MacOsUsbDevice): completed the native mapping

--- library/src/commonMain/kotlin/com/tecknobit/kinfo/model/desktop/macos/hardware/MacOsUsbDevice.kt
+++ library/src/commonMain/kotlin/com/tecknobit/kinfo/model/desktop/macos/hardware/MacOsUsbDevice.kt
@@ -2,4 +2,13 @@ package com.tecknobit.kinfo.model.desktop.macos.hardware
 
 import com.tecknobit.kinfo.model.desktop.common.hardware.UsbDevice
 
+/**
+ * The `MacOsUsbDevice` interface defines the contract to expose macOS USB device information through [UsbDevice]
+ *
+ * The native mapping uses a registry entry identifier as the device identity, which is not persistent across reboots
+ *
+ * @author N7ghtm4r3 - Tecknobit
+ *
+ * @since 1.1.0
+ */
 interface MacOsUsbDevice : UsbDevice
